2e6619161efa0aa86f05cb9748e023e35870d62e
Ivan Bazina <ibazina@int.sedamIT.hr>
2022-02-01 21:05:39 +0100

    Add dialects/list and dialects/list/[max] JSON APIs

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dialects-be
 Backend app for "dialects" mini project.
+
+
+## Requirements:
+1. Create "docs/" folder
+2. Put "data.xlsx" file inside "docs/" folder (file contains dialects data)
--- routes/dialects.js
+++ routes/dialects.js
@@ -1,7 +1,33 @@
 const router = require('express').Router();
+const readXlsxFile = require('read-excel-file/node');
 
 router.get('/', function(req, res) {
     res.send('Dialects Page');
+});
+
+router.get('/list', function(req, res) {
+    let jsonRsp = {
+        data: {}
+    };
+
+    readXlsxFile('docs/data.xlsx').then((rows) => {
+        jsonRsp.data = rows;
+        res.set('Content-Type', 'application/json');
+        res.send(JSON.stringify(jsonRsp));
+    });
+});
+
+router.get('/list/:max', function(req, res) {
+    let jsonRsp = {
+        data: {}
+    };
+
+    readXlsxFile('docs/data.xlsx').then((rows) => {
+        let max = req.params.max;
+        (rows.length > max) ? jsonRsp.data = rows.slice(0, max) : jsonRsp.data = rows;
+        res.set('Content-Type', 'application/json');
+        res.send(JSON.stringify(jsonRsp));
+    });
 });
 
 module.exports = router;
